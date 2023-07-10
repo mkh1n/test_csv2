f655e939ad08c1bf9c671d2dbecd08dd3c804727
Roman Mukhin <romamuhino1@gmail.com>
2023-07-11 00:26:02 +0300

    test

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 import _ from 'lodash';
 
 export default function solution(content){
-  // BEGIN
-  console.log(content);
-  // END
+  const contentArr = content.trim().split('\n').slice(1).map((row)=>row.split(','))
+  //Первый шаг
+  console.log(`Количество партий: ${contentArr.length}`)
+
+  //Второй шаг
+  const ratedCount = contentArr.filter((game)=>game[1] == 'TRUE').length
+  const nonRatedCount = contentArr.length - ratedCount
+  console.log(`Рейтинговых игр: ${((ratedCount/contentArr.length)*100).toFixed(2)}%`)
+  console.log(`Нерейтинговых игр игр: ${((nonRatedCount/contentArr.length)*100).toFixed(2)}%`)
+
+  //Третий шаг
+  const debutes = Array(...new Set(contentArr.map((game)=>game[13]))).sort()
+  console.log(debutes)
 }
